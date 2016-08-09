b2d3152287de6957f0f5a4bf689111d5a07bca9b
Ofaloaf <mkundakco08@charter.net>
2016-08-08 22:08:31 -0400

    Major revisions
    -Removed some colonial revolters temporarily
    -Reworked religions, partially just relocalized
    -Updated to reflect 1402 endpoint of CK2 LP
    -Vote considerations added in
    -4 centers of reformation, wew

---
+++ common/defines/gothlp_defines.lua
@@ -0,0 +1,5 @@
+NDefines.NGame.START_DATE = "1402.3.12"
+NDefines.NReligion.MAX_CHRISTIAN_RELIGIOUS_CENTERS = 4
+NDefines.NReligion.MAX_CARDINALS = 60										-- Max amount of cardinals
+NDefines.NReligion.MAX_CARDINALS_PER_COUNTRY = 10							-- Max cardinals in a single country
+NDefines.NReligion.REFORM_DESIRE_PER_YEAR = 0.003
